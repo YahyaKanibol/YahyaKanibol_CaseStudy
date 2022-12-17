0703dce49f23265499b324491264ca94ed7b5c4a
Yahya <yahyakanibol@gmail.com>
2022-12-17 22:23:43 +0300

    Project is ready

--- src/test/java/com/techCrunch/pages/ImagesPage.java
+++ src/test/java/com/techCrunch/pages/ImagesPage.java
@@ -15,8 +15,6 @@ public class ImagesPage {
     /**
      * There are bunch of Latest News,each news should have an "Image", so they all stored in  as List.
      **/
-
-
     public void imageVerification() {
         List<WebElement> allThumbs = Driver.getDriver().findElements(By.xpath("//img[@sizes='(min-width: 1024px) 430px, 100vw']"));
         //IsImageDisplayed Method checks if the image exist on the addressed locator.
