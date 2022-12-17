1e614e508c50b7261e8a6e725f46c1512a8fa3a3
Yahya <yahyakanibol@gmail.com>
2022-12-17 17:19:51 +0300

    all drafted

--- src/test/java/com/techCrunch/pages/LatestNews.java
+++ src/test/java/com/techCrunch/pages/LatestNews.java
@@ -1,15 +1,96 @@
 package com.techCrunch.pages;
 
 import com.techCrunch.utilities.Driver;
+import com.techCrunch.utilities.Utilities;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class LatestNews {
     public LatestNews() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
- /*Since all the web elements are required to store in lists, there is no single WebElement to be stored in Pages package. */
+    String browserTitle = "";
+    String onlyBrowserTitle = "";
+    String currentURL = "";
+    String newsTitleBeforeClick = "";
+    String urlAsString = "";
+    String browserTitlePure = "";
+    String[] eachWord_of_the_URL = null;
+    String[] eachWord_of_the_BrowserTitle = null;
+
+    // There are bunch of Latest News, so they all stored in latestNews as List.
+    List<WebElement> latestNews = Driver.getDriver().findElements(By.xpath("//article[contains(@class,'post-block')]"));
+
+    public void get_all_the_LatestNews_collectData() {
+        // In order to select random from the Latest News, Random is used.
+        Random random = new Random();
+        int randomIndex = random.nextInt(latestNews.size());
+
+        //One of the news is selected, and we should get the header before click the news.
+        newsTitleBeforeClick = (latestNews.get(randomIndex)).getText();
+
+        //Selected news is clicked
+        latestNews.get(randomIndex).click();
+
+        //After clicked on the news, the title of the page is stored as "browserTitle"
+        browserTitle = Driver.getDriver().getTitle();
+
+        //Browser title is needed to get rid of "TechCruch" brand to compare with News Title
+        onlyBrowserTitle = browserTitle.substring(0, (browserTitle.indexOf("|") - 1));
+
+        //Current URL is needed to get it's content
+        currentURL = Driver.getDriver().getCurrentUrl();
+
+        //Current URL needs to get rid of protocol and service name, URLToString method is created and used for this purpose.
+        urlAsString = Utilities.URLToString(currentURL);
+
+        //Current URL needs to be stored in an Array as words to be compared with Browser title,
+        eachWord_of_the_URL = urlAsString.split(" ");
+
+        //Browser title without special characters,
+        browserTitlePure = onlyBrowserTitle.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
+        //Browser title needs to be stored in array as words to be compared with news' URL
+        eachWord_of_the_BrowserTitle = browserTitlePure.split(" ");
+
+        System.out.println("***RANDOM INDEX= " + randomIndex);
+        System.out.println(" ");
+        System.out.println("***NEWS TITLE = " + newsTitleBeforeClick);
+        System.out.println(" ");
+        System.out.println("***BROWSER TITLE = " + browserTitle);
+        System.out.println(" ");
+        System.out.println("***BROWSER TITLE AS WITHOUT BRAND= " + onlyBrowserTitle);
+        System.out.println(" ");
+        System.out.println("***CURRENT URL = " + currentURL);
+        System.out.println(" ");
+        System.out.println("***URL AS STRING = " + urlAsString);
+        System.out.println(" ");
+        System.out.println("***BrowserTitlePure = " + browserTitlePure);
+        System.out.println(" ");
+        System.out.println("[URL as Array]= " + Arrays.toString(eachWord_of_the_URL));
+        System.out.println(" ");
+        System.out.println("[Browser Title in Array] = " + Arrays.toString(eachWord_of_the_BrowserTitle));
+
+    }
+
+    public void assert_and_verify() {
+        /*Firstly, we verify that browser title is exactly contained in news title*/
+        Assert.assertTrue(newsTitleBeforeClick.contains(onlyBrowserTitle));
+
+        /*Secondly, we verify that the words of news' URL matches with news' content (News content is inherited from browser's title)*/
+        Assert.assertTrue(Utilities.ElementsInCommon(eachWord_of_the_URL, eachWord_of_the_BrowserTitle));
+
+    }
+
+
+
+    /*Since all the web elements are required to store in lists, there is no single WebElement to be stored in Pages package. */
 
 }
--- src/test/java/com/techCrunch/step_definitions/secondScenerio_stepDefs.java
+++ src/test/java/com/techCrunch/step_definitions/secondScenerio_stepDefs.java
@@ -1,5 +1,6 @@
 package com.techCrunch.step_definitions;
 
+import com.techCrunch.pages.LatestNews;
 import com.techCrunch.utilities.Driver;
 import com.techCrunch.utilities.Utilities;
 import io.cucumber.java.en.Then;
@@ -7,64 +8,21 @@ import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class secondScenerio_stepDefs {
 
-
-    String browserTitle = "";
-    String browserTitleAsString ="";
-    String currentURL = "";
-    String newsTitle = "";
-    String urlAsString="";
-    String expectBrowserTitlePure="";
-    String[] eachWord_of_the_URL =null;
-    String[] eachWord_of_the_BrowserTitle =null;
-
+    LatestNews latestNews=new LatestNews();
     @When("User clicks randomly one of the news from Latest News")
     public void user_clicks_randomly_one_of_the_news_from_latest_news() {
-        List<WebElement> latestNews = Driver.getDriver().findElements(By.xpath("//article[contains(@class,'post-block')]"));
-        Random random= new Random();
-        int randomIndex = random.nextInt(latestNews.size());
-
-        newsTitle = (latestNews.get(randomIndex)).getText();
-        latestNews.get(randomIndex).click();
-        browserTitle = Driver.getDriver().getTitle();
-        browserTitleAsString =browserTitle.substring(0,(browserTitle.indexOf("|")-1));
-        currentURL = Driver.getDriver().getCurrentUrl();
-        urlAsString=Utilities.URLToString(currentURL);
-        eachWord_of_the_URL = urlAsString.split(" ");
-        expectBrowserTitlePure = browserTitleAsString.replaceAll("[^a-zA-Z0-9 ]", "").toLowerCase();
-        eachWord_of_the_BrowserTitle = expectBrowserTitlePure.split(" ");
-
-        System.out.println("***RANDOM INDEX= " + randomIndex);
-        System.out.println(" ");
-        System.out.println("***NEWS TITLE = " + newsTitle);
-        System.out.println(" ");
-        System.out.println("***BROWSER TITLE = " + browserTitle);
-        System.out.println(" ");
-        System.out.println("***CURRENT URL = " + currentURL);
-        System.out.println(" ");
-        System.out.println("***URL AS STRING = " + urlAsString);
-        System.out.println(" ");
-        System.out.println("***BROWSER TITLE AS STRING= " + browserTitleAsString);
-        System.out.println(" ");
-        System.out.println("expectBrowserTitlePure = " + expectBrowserTitlePure);
-        System.out.println(" ");
-        System.out.println("URL as Array= " + Arrays.toString(eachWord_of_the_URL));
-        System.out.println(" ");
-        System.out.println("Browser Title in Array = " + Arrays.toString(eachWord_of_the_BrowserTitle));
-
+        latestNews.get_all_the_LatestNews_collectData();
     }
 
     @Then("Verify that the browser title is the same with the news title and the links within the news content")
     public void verify_that_the_browser_title_is_the_same_with_the_news_title_and_the_links_within_the_news_content() {
-        /*Firstly, we verify that browser title is exactly contained in news title*/
-        Assert.assertTrue(newsTitle.contains(browserTitleAsString));
-
-        /*Secondly, we verify that the words of news' URL matches with news' content (News content is inherited from browser's title)*/
-        Assert.assertTrue(Utilities.ElementsInCommon(eachWord_of_the_URL, eachWord_of_the_BrowserTitle));
+        latestNews.assert_and_verify();
     }
 }
