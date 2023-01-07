4c60a9e091869b044769f19ec4936e15add8d34f
Yahya <yahyakanibol@gmail.com>
2023-01-08 02:18:46 +0300

    Project is ready

--- src/test/java/com/techCrunch/utilities/Driver.java
+++ src/test/java/com/techCrunch/utilities/Driver.java
@@ -4,7 +4,10 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
@@ -19,6 +22,22 @@ public class Driver {
             String browserType = ConfigurationReader.getProperty("browser");
 
             switch (browserType){
+                case "remote-chrome":
+                    try {
+                        // assign your grid server address
+                        String gridAddress = "54.205.45.102";
+                        URL url = new URL("http://" + gridAddress + ":8081/wd/hub");
+                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                        desiredCapabilities.setBrowserName("chrome");
+                        driverPool.set(new RemoteWebDriver(url,desiredCapabilities));
+                        driverPool.get().manage().window().maximize();
+                        driverPool.get().manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
                     driverPool.set(new ChromeDriver());
